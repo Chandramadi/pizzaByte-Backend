908be389a8f6b32944d2ddf11c0dca5f65a39848
chandramadi <chandramadi6121@gmail.com>
2024-10-29 23:22:24 +0530

    Added Product Schema

---
+++ src/schema/productSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    productName : {
+        type : String,
+        required : [true, "Product name is required"],
+        minLength : [6,"Product description must be at least 6 characters."],
+        trim:true,
+    },
+    description : {
+        type : String,
+        minLength : [6,"Product description must be at least 6 characters."]
+    },
+    productImage : {
+        type : String,
+    },
+    price : {
+        type : Number,
+        required : [true, "Product price is required"]
+    },
+    category : {
+        type : String,
+        enum : ['veg','non-veg','drinks','slides'],
+        default : 'veg',
+    },
+    inStock : {
+        type : Boolean,
+        required : [true, "In stock staus is required."],
+        default : true,
+    },
+},{
+    timestamps:true,
+})
+
+
+const Product = mongoose.model("Product",productSchema);
+
+module.exports = Product;
