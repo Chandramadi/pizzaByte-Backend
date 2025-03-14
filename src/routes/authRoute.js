d9ed4f60d13d88bd6ca77dc1bce4f547e111a8fa
chandramadi <chandramadi6121@gmail.com>
2025-03-14 19:14:34 +0530

    added isLoggedIn route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { authController, logout}= require("../controllers/authController");
+const { isLoggedIn } = require("../validation/authValidator");
 
 const authRoute = express.Router();
 
 authRoute.post("/login",authController);
 
 authRoute.post("/logout",logout);
+
+authRoute.get("/isLoggedIn", isLoggedIn, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "User is logged in",
+        error: {},
+        data : {},
+    });
+});
 
 module.exports = {
     authRoute,
